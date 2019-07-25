58e2b87e1916f03bcdef83eff95aaa78816607cd
Hui Lin <longqiman@gmail.com>
2019-07-24 18:51:25 -0700

    Update set_revenue_category().R

--- R/set_revenue_category().R
+++ R/set_revenue_category().R
@@ -6,7 +6,7 @@ set_revenue_category <- function () {
   assign("legacy", c("domain", "site", "ssl", "bitballoon-monthly-june-2013", "Legacy Netlify Sites",
                    "bitballoon-yearly-june-2013", "automation"), envir = .GlobalEnv)
   assign("inside", c("Enterprise plans", "Reseller plans","Unlimited-Identity"), envir = .GlobalEnv)
-  assign("add_ons", c("Forms", "Functions", "Identity", "Domains","Large Media", "Analytics", "Extra team members"), envir = .GlobalEnv)
+  assign("add_ons", c("Forms", "Functions", "Identity", "Domains","Large Media", "Analytics"), envir = .GlobalEnv)
   assign("core", c(team, "Extra team members"), envir = .GlobalEnv)
   assign("selfServe", c(core, legacy, add_ons), envir = .GlobalEnv)
 }
